9aa17ad01bea870044f4921240de6d9c1912ce5e
aldrichdungo26-png <aldrichdungo26@gmail.com>
2026-05-24 14:10:54 +0800

    migrate

---
+++ resources/views/student/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">{{ __('Update Profile') }}</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+<!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="alert alert-info">
+                        This is the form for updating your profile
+                    </div>
+
+                    <div class="card">
+                        <div class="card-body p-0">
+
+                            <form action="{{ route('profile.update') }}" method="POST">
+                                @csrf
+                                @method('PUT')
+
+                                <div class="form-group">
+                                    <label for="fname">First Name</label>
+                                    <input type="text" name="fname" id="fname" class="form-control" value="{{ auth()->user()->fname }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="lname">Last Name</label>
+                                    <input type="text" name="lname" id="lname" class="form-control" value="{{ auth()->user()->lname }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="midname">Middle Name</label>
+                                    <input type="text" name="midname" id="midname" class="form-control" value="{{ auth()->user()->midname }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="age">Age</label>
+                                    <input type="number" name="age" id="age" class="form-control" value="{{ auth()->user()->age }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                                    <input type="text" name="address" id="address" class="form-control" value="{{ auth()->user()->address }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="zipcode">Zip Code</label>
+                                    <input type="text" name="zipcode" id="zipcode" class="form-control" value="{{ auth()->user()->zipcode }}">
+                                </div>
